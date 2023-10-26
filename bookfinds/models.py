78821f170f7743c0064cc860e5fa69a88ece4e8c
nadyaaysha <nadyaaysha07@gmail.com>
2023-10-26 09:17:21 +0700

    Edit

--- bookfinds/models.py
+++ bookfinds/models.py
@@ -1,8 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import User
 
 class Book(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
     isbn = models.CharField(max_length=10)
     title = models.CharField(max_length=255)
     subtitle = models.CharField(max_length=255)
