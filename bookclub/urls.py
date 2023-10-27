c6a61468c2cdc12d2c5c6dd0ea4615ae985a3727
eelizabethmichelle <michellelizabethah@gmail.com>
2023-10-27 23:36:42 +0700

    Implement AJAX

--- bookclub/urls.py
+++ bookclub/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from bookclub.views import show_main, create_club, delete_club, join_club, leave_club, show_club
+from bookclub.views import get_club_json, get_bubble_json, get_book_json
+from bookclub.views import create_club_ajax
 from bookclub.views import post_bubble, add_rec_book
+from bookclub.views import is_owner, is_member
 from RegLogInOut.views import register, login_user, logout_user
 
 app_name = 'book-club'
@@ -17,4 +20,10 @@ urlpatterns = [
     path('show-club/<int:club_id>/', show_club, name='show_club'),
     path('post-bubble/<int:club_id>/', post_bubble, name='post_bubble'),
     path('add-rec-book/<int:club_id>/', add_rec_book, name='add_rec_book'),
+    path('get-club-json', get_club_json, name='get_club_json'),
+    path('get-bubble-json/<int:club_id>/', get_bubble_json, name='get_bubble_json'),
+    path('get-book-json', get_book_json, name='get_book_json'),
+    path('create-club-ajax', create_club_ajax, name='create_club_ajax'),
+    path('is-owner/<int:club_id>/', is_owner, name='is_owner'),
+    path('is-member/<int:club_id>/', is_member, name='is_member'),
 ]
--- bookclub/views.py
+++ bookclub/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponseRedirect
-from bookclub.models import Club, Bubble
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseNotFound
+from django.core import serializers
+from bookclub.models import Club, Book, Bubble
 from bookclub.forms import ClubForm, BubbleForm, BookRecForm
 from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt 
+
+def get_club_json(request):
+    clubs = Club.objects.all()
+    return HttpResponse(serializers.serialize('json', clubs))
+
+def get_bubble_json(request, club_id):
+    bubbles = Bubble.objects.filter(id=club_id)
+    return HttpResponse(serializers.serialize('json', bubbles))
+
+def get_book_json(request):
+    books = Book.objects.all()
+    return HttpResponse(serializers.serialize('json', books))
 
 def show_main(request):
     clubs = Club.objects.all()
@@ -26,32 +40,77 @@ def create_club(request):
     context = {'form': form}
     return render(request, "create_club.html", context)
 
-@login_required(login_url='/login')
-def delete_club(request, club_id):
-    club = get_object_or_404(Club, id=club_id)
+@csrf_exempt
+def create_club_ajax(request):
+    if request.method == 'POST':
+        user = request.user
+        name = request.POST.get("name")
+        description = request.POST.get("description")
+        recommended_books = request.POST.get("recommended_books")
 
-    if request.user == club.owner:
+        new_club = Club(owner=user, name=name, description=description)
+        new_club.save()
+        new_club.members.add(user)
+        new_club.recommended_books.add(recommended_books)
+
+        bubble = request.POST.get("bubble")
+
+        new_bubble = Bubble(user=user, club=new_club, content=bubble)
+        new_bubble.save()
+
+        return HttpResponse(b"CREATED", status=201)   
+    
+    return HttpResponseNotFound
+
+@csrf_exempt
+def delete_club(request, club_id):
+    if request.method == 'POST':
+        club = get_object_or_404(Club, id=club_id)
         club.delete()
 
-    return redirect('book-club:show_main')
+        return HttpResponse(b"OK", status=200)
 
-@login_required(login_url='/login')
+    return HttpResponseNotFound
+
+@csrf_exempt
 def join_club(request, club_id):
-    club = get_object_or_404(Club, id=club_id)
-
-    if request.user not in club.members.all():
+    if request.method == 'POST':
+        club = get_object_or_404(Club, id=club_id)
         club.members.add(request.user)
 
-    return redirect('book-club:show_main')
+        return HttpResponse(b"OK", status=200)
 
-@login_required(login_url='/login')
+    return HttpResponseNotFound
+
+@csrf_exempt
 def leave_club(request, club_id):
-    club = get_object_or_404(Club, id=club_id)
-
-    if request.user in club.members.all():
+    if request.method == 'POST':
+        club = get_object_or_404(Club, id=club_id)
         club.members.remove(request.user)
 
-    return redirect('book-club:show_main')
+        return HttpResponse(b"OK", status=200)
+
+    return HttpResponseNotFound
+
+@csrf_exempt
+def is_owner(request, club_id):
+    if request.method == 'POST':
+        club = get_object_or_404(Club, id=club_id)
+        
+        if request.user == club.owner:
+            return HttpResponse('True', content_type='text/plain')
+    
+    return HttpResponse('False', content_type='text/plain')
+
+@csrf_exempt
+def is_member(request, club_id):
+    if request.method == 'POST':
+        club = get_object_or_404(Club, id=club_id)
+        
+        if request.user in club.members.all():
+            return HttpResponse('True', content_type='text/plain')
+    
+    return HttpResponse('False', content_type='text/plain')
 
 @login_required(login_url='/login')
 def show_club(request, club_id):
