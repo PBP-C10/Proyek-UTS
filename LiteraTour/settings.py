99c2e2d992d9f7359eb799f914ed60455612f3f6
nadyaaysha <nadyaaysha07@gmail.com>
2023-10-28 17:18:24 +0700

    Delete Old

--- LiteraTour/settings.py
+++ LiteraTour/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'signInSignUp',
     'bookfinds',
-    'bookclub'
+    'bookclub',
     'myLibrary',
 ]
 
--- MyLib/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from MyLib.models import MyBooks
-
-class BookTrackForm(ModelForm):
-    class Meta:
-        model = MyBooks
-        fields = ["my_Book", "progress", "quote"]
