2340f424832a600d158407cff48875798f467eb8
Roger Moreno <roger.moreno@ui.ac.id>
2023-10-29 18:41:56 +0700

    fix merged conflict

--- BookShop/views.py
+++ BookShop/views.py
@@ -4,6 +4,57 @@ from BookShop.models import Cart, Order
 from bookfinds.models import Book
 from django.contrib import messages
 from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+from django.db.models import Q
+from django.core import serializers
+from django.core.paginator import Paginator
+
+def filter_books(request):
+    books = Book.objects.all()
+
+
+    min_price = request.GET.get('minPrice')
+    max_price = request.GET.get('maxPrice')
+
+    if not min_price:
+        min_price = 0
+
+    if not max_price:
+        max_price = 500000
+
+    books = books.filter(price__range=[min_price, max_price])
+
+    min_rating = request.GET.get('minRating')
+    if min_rating:
+        books = books.filter(average_rating__range=[min_rating,5])
+
+    page = request.GET.get('page')
+    if not page:
+        page = 1
+    paginator = Paginator(books, 30)
+    booksInPage = paginator.get_page(page)
+
+    if booksInPage.has_next():
+        next_page = booksInPage.next_page_number()
+    else:
+        next_page = None
+
+    if booksInPage.has_previous():
+        prev_page = booksInPage.previous_page_number()
+    else:
+        prev_page = None
+
+    data = {
+        'total_page_num': paginator.num_pages,
+        'current_page': int(page),
+        'has_next': booksInPage.has_next(),
+        'next_page': next_page,
+        'has_previous': booksInPage.has_previous(),
+        'prev_page': prev_page,
+        'books': serializers.serialize('json', booksInPage),
+    }
+    return JsonResponse(data)    
+
 
 def shopping_main(request):
     if not request.user.is_anonymous:
