627aa6ccdd4933bfa65d58a2dbe9c2270e14de24
eelizabethmichelle <michellelizabethah@gmail.com>
2023-10-29 16:58:43 +0700

    Separate js

--- LiteraTour/settings.py
+++ LiteraTour/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'signInSignUp',
     'bookfinds',
     'bookclub',
-    'BookShop'
+    'BookShop',
 ]
 
 MIDDLEWARE = [
@@ -122,7 +122,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'bookfinds\static')
+    os.path.join(BASE_DIR, 'bookfinds\static'),
+    os.path.join(BASE_DIR, 'bookclub\static'),
 ]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Default primary key field type
